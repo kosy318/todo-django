ef9a98d3313a3f30c6c1c1b0b2e807a89f2b3e3b
kosy318 <77595685+kosy318@users.noreply.github.com>
2023-06-02 15:37:05 +0900

    todo update

--- todo/board/urls.py
+++ todo/board/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.Todo_board.as_view(), name='board'),
     url(r'^insert/$', views.check_post, name='board_insert'),
     url(r'^(?P<pk>[0-9]+)/detail/$', views.Board_detail.as_view(), name='board_detail'),
+    url(r'^(?P<pk>[0-9]+)/update/$', views.Board_update.as_view(), name='board_update'),
 ]
--- todo/board/views.py
+++ todo/board/views.py
@@ -19,6 +19,27 @@ class Board_detail(generic.DetailView):
     context_object_name = 'todo'
 
 
+class Board_update(generic.UpdateView):
+    model = TodoList
+    fields = ('title', 'content', 'end_date')
+    template_name = 'board/board_update.html'
+    success_url = '/board/'
+
+    def form_valid(self, form):
+        form.save()
+        return render(self.request, 'board/succeed.html', {"message": "일정을 업데이트 했습니다"})
+
+    # 상세보기 -> 수정버튼 눌렀을 때 기존에 작성한 내용이 적용되어야함
+    def get(self, request, *args, **kwargs):
+        # 오브젝트를 받아와서 폼 클래스를 받아온 후 return
+        self.object = self.get_object()
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+
+        context = self.get_context_data(object=self.object, form=form)
+        return self.render_to_response(context)
+
+
 def check_post(request):
     if request.method == 'POST':
         template_name = 'board/succeed.html'
